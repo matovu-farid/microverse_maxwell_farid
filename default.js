d6296723293372b31bf65259e04c4e76cde4398f
Farid <matovu90@gmail.com>
2021-10-23 18:22:35 +0300

    added buttons

---
+++ default.js
@@ -0,0 +1,10 @@
+var elemennt = document.getElementById("install");
+elemennt.addEventListener('click',()=>{
+window.open('https://play.google.com/store/apps/details?id=com.faridmatovu.painter_app','_blank').focus()
+})
+
+var elemennt = document.getElementById("restaurant_button");
+elemennt.addEventListener('click',()=>{
+window.open('hhttps://restaurantconfusion.netlify.app','_blank').focus()
+})
+//https://restaurantconfusion.netlify.app
